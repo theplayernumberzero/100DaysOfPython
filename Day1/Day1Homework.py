8ec39188e2456e1cab6918a1cec64ab536c7b988
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-11-14 00:29:39 +0300

    Day1

---
+++ Day1/Day1Homework.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city = input("Which city did you grow up when you child: ")
+pet= input("What is the name of your pet: ")
+print("Your band name could be: " + city + " " + pet)
---
+++ Day1/PrintAndInputFunction.py
@@ -0,0 +1,1 @@
+print("Hello " + input("Type your name: "))
